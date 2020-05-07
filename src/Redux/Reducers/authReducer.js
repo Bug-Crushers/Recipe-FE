e07fc879b49b64363231cd1e4e3cbaed19110fcb
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-05-07 12:39:14 +0100

    created the register reducer

--- src/Redux/Reducers/authReducer.js
+++ src/Redux/Reducers/authReducer.js
@@ -9,14 +9,21 @@ const initialState = {
 
 function authReducer(state = initialState, action) {
     switch (action.type) {
+        case (types.REGISTER_START):
+            return { ...state, loading: true };
         case (types.LOGIN_START):
             return { ...state, loading: true };
 
+        case (types.REGISTER_SUCCESS):
+            return { ...state, token: action.payload, loading: false }
         case (types.LOGIN_SUCCESS):
             return { ...state, token: action.payload, loading: false };
 
-        case (types.LOGIN_FAILURE):
+        case (types.REGISTER_FAILURE):
             return { ...state, error: true, loading: false }
+        case (types.LOGIN_FAILURE):
+            return { ...state, error: true, loading: false };
+
         default:
             return state;
     }
