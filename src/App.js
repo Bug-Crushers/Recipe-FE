605928c1d2cb56a7c9f6f6b65b449ab9e30af0b2
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-05-07 13:17:41 +0100

    added routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login';
+import Register from './Components/Register';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       Welcome to secret family recipe book
-      <Login/>
+    <Route exact path='/' component={Login}/>
+    <Route exact path='/register' component={Register} />    
+
     </div>
   );
 }
