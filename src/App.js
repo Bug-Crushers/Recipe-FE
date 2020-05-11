8129ee941a76af3e5aa75fe21a4393652e0d5ac7
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-05-11 20:19:13 +0100

    added facebook auth

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import { Route } from 'react-router-dom';
+import Facebook from './Components/Facebook';
 
 function App() {
   return (
     <div className="App">
       Welcome to secret family recipe book
     <Route exact path='/' component={Login}/>
-    <Route exact path='/register' component={Register} />    
+    <Route exact path='/register' component={Register} />  
+    <Facebook/>  
 
     </div>
   );
