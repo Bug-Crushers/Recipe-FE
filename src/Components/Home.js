79211b2fc60f1e5262c699ee0aad3e38cd30e925
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-05-27 19:09:39 +0100

    added redirect and authheck funtion

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home (){
+    return (
+        <div>
+            Yooooo this is the Home component...... you must be signed in to see this
+
+        </div>
+    )
+}
+
+export default Home;
